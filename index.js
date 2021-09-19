bd7a158878d2802c9c7e0ad483d63ffe794e96d4
Saurabh Burewar <burewar.1@iitj.ac.in>
2021-09-19 21:19:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+
+tl.to('.name span', {y: "0%", duration: 1, stagger: 0.25});
+// tl.to('.name', {y: "-1190%", duration: 1, stagger: 0.25});
+// tl.to('.name1', {y: "-1170%", duration: 1, stagger: 0.25}, "-=1");
+// tl.to('.name', {fontSize: 20, duration: 1, stagger: 0.25}, "-=1");
+// tl.to('.name', {height: "7%", duration: 1, stagger: 0.25}, "-=2");
+// tl.to('.name1', {fontSize: 15, duration: 1, stagger: 0.25}, "-=1");
+tl.to('.intro', {opacity: 0, duration: 1.5, delay: 1});
+tl.to('.intro', {zIndex:-100});
+
+document.getElementById('headAbout').addEventListener("mousedown", function() {
+    document.getElementById('displayAbout').style.display = "flex";
+    document.getElementById('displaySkills').style.display = "none";
+    document.getElementById('displayProject').style.display = "none";
+    document.getElementById('displayExp').style.display = "none";
+    document.getElementById('displayContact').style.display = "none";
+    document.getElementById('headAbout').classList.add("navselected");
+    document.getElementById('headSkill').classList.remove("navselected");
+    document.getElementById('headProject').classList.remove("navselected");
+    document.getElementById('headExp').classList.remove("navselected");
+    document.getElementById('headContact').classList.remove("navselected");
+})
+
+document.getElementById('headSkill').addEventListener("mousedown", function() {
+    document.getElementById('displayAbout').style.display = "none";
+    document.getElementById('displaySkills').style.display = "flex";
+    document.getElementById('displayProject').style.display = "none";
+    document.getElementById('displayExp').style.display = "none";
+    document.getElementById('displayContact').style.display = "none";
+    document.getElementById('headAbout').classList.remove("navselected");
+    document.getElementById('headSkill').classList.add("navselected");
+    document.getElementById('headProject').classList.remove("navselected");
+    document.getElementById('headExp').classList.remove("navselected");
+    document.getElementById('headContact').classList.remove("navselected");
+})
+
+document.getElementById('headProject').addEventListener("mousedown", function() {
+    document.getElementById('displayAbout').style.display = "none";
+    document.getElementById('displaySkills').style.display = "none";
+    document.getElementById('displayProject').style.display = "block";
+    document.getElementById('displayExp').style.display = "none";
+    document.getElementById('displayContact').style.display = "none";
+    document.getElementById('headAbout').classList.remove("navselected");
+    document.getElementById('headSkill').classList.remove("navselected");
+    document.getElementById('headProject').classList.add("navselected");
+    document.getElementById('headExp').classList.remove("navselected");
+    document.getElementById('headContact').classList.remove("navselected");
+})
+
+document.getElementById('headExp').addEventListener("mousedown", function() {
+    document.getElementById('displayAbout').style.display = "none";
+    document.getElementById('displaySkills').style.display = "none";
+    document.getElementById('displayProject').style.display = "none";
+    document.getElementById('displayExp').style.display = "block";
+    document.getElementById('displayContact').style.display = "none";
+    document.getElementById('headAbout').classList.remove("navselected");
+    document.getElementById('headSkill').classList.remove("navselected");
+    document.getElementById('headProject').classList.remove("navselected");
+    document.getElementById('headExp').classList.add("navselected");
+    document.getElementById('headContact').classList.remove("navselected");
+})
+document.getElementById('pmlink').addEventListener("mousedown", function() {
+    document.getElementById('displayAbout').style.display = "none";
+    document.getElementById('displaySkills').style.display = "none";
+    document.getElementById('displayProject').style.display = "none";
+    document.getElementById('displayExp').style.display = "block";
+    document.getElementById('displayContact').style.display = "none";
+    document.getElementById('headAbout').classList.remove("navselected");
+    document.getElementById('headSkill').classList.remove("navselected");
+    document.getElementById('headProject').classList.remove("navselected");
+    document.getElementById('headExp').classList.add("navselected");
+    document.getElementById('headContact').classList.remove("navselected");
+})
+
+document.getElementById('headContact').addEventListener("mousedown", function() {
+    document.getElementById('displayAbout').style.display = "none";
+    document.getElementById('displaySkills').style.display = "none";
+    document.getElementById('displayProject').style.display = "none";
+    document.getElementById('displayExp').style.display = "none";
+    document.getElementById('displayContact').style.display = "flex";
+    document.getElementById('headAbout').classList.remove("navselected");
+    document.getElementById('headSkill').classList.remove("navselected");
+    document.getElementById('headProject').classList.remove("navselected");
+    document.getElementById('headExp').classList.remove("navselected");
+    document.getElementById('headContact').classList.add("navselected");
+})
